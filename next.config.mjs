1650a48e61cb1a5294502c837dc3612322ccf6df
nosheenmustafa <nosheenmustafa21@gmail.com>
2025-05-07 12:13:20 +0500

    new change

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com",  //for google profile picture
-      "avatars.githubusercontent.com",   //for github profile picture 
-      "platform-lookaside.fbsbx.com", //for facebook profile picture 
+    domains: [
+      "lh3.googleusercontent.com",         // Google profile pictures
+      "avatars.githubusercontent.com",     // GitHub profile pictures
+      "platform-lookaside.fbsbx.com",      // Facebook profile pictures
+      "res.cloudinary.com",                // ✅ Cloudinary domain
     ],
   },
 };
